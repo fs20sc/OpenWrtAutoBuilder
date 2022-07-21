192d8f0ef8fe1198a7c5e41e0ad54aa0cfa0da28
fs20sc <fs20@dobest.org>
2022-07-21 16:17:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# fix JCG Q20
+#sed -i -e '/define Device\/jcg_q20/{n;s|$|\n  $(Device/uimage-lzma-loader)|}' target/linux/ramips/image/mt7621.mk
